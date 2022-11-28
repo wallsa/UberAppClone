3bbd41084866e53ce1c3bf1c29dc88040dfa89a9
Wallace Santos <wallace_bcc@hotmail.com>
2022-11-27 22:33:16 -0300

    Implementado as anotações que representam os motoristas e atualizando a localização mediante a mudança na base de dados

--- UberAppClone/Model/DriverAnnotation.swift
+++ UberAppClone/Model/DriverAnnotation.swift
@@ -9,11 +9,18 @@ import MapKit
 
 
 class DriverAnnotation: NSObject, MKAnnotation{
-    var coordinate: CLLocationCoordinate2D
+    //Dynamic keyword usada para que a atualizacao da coordenada seja atualizada a medida que mudamos na Database
+    dynamic var coordinate: CLLocationCoordinate2D
     var uid:String
     
     init (uid: String, coordinate:CLLocationCoordinate2D) {
         self.uid = uid
         self.coordinate = coordinate
-    }  
+    }
+    
+    func updateAnnotationPosition(withCoordinate coordinate: CLLocationCoordinate2D){
+        UIView.animate(withDuration: 0.2) {
+            self.coordinate = coordinate
+        }
+    }
 }
